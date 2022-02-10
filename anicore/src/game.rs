054514a39272b24772ec76a2b8781262bfb58cb8
Twilight-Struggle <pikupiken@yahoo.co.jp>
2022-02-11 03:57:27 +0900

    途中まで更新

---
+++ anicore/src/game.rs
@@ -0,0 +1,382 @@
+use crate::Act;
+use crate::Agent;
+use rand::Rng;
+
+const HEIGHT: isize = 5;
+const WIDTH: isize = 3;
+const SARU_KICK: [[isize; 2]; 6] = [[1, 1], [1, -1], [-1, 0], [2, 2], [2, -2], [-2, 0]];
+
+#[derive(Debug, Clone, PartialEq, Eq)]
+enum PieceKind {
+    Saru([isize; 2], Vec<[isize; 2]>),
+    Usagi([isize; 2], Vec<[isize; 2]>),
+    Risu([isize; 2], Vec<[isize; 2]>),
+    Oyasaru([isize; 2], Vec<[isize; 2]>),
+    Ball,
+}
+impl PieceKind {
+    fn get(&self) -> Option<(&[isize; 2], &Vec<[isize; 2]>)> {
+        match self {
+            PieceKind::Saru(idou, kick) => Some((idou, kick)),
+            PieceKind::Usagi(idou, kick) => Some((idou, kick)),
+            PieceKind::Risu(idou, kick) => Some((idou, kick)),
+            PieceKind::Oyasaru(idou, kick) => Some((idou, kick)),
+            _ => None,
+        }
+    }
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+enum Player {
+    Attack,
+    Defence,
+    Neutral,
+}
+impl Player {
+    fn against(&self) -> Self {
+        if self == &Self::Attack {
+            Self::Defence
+        } else {
+            Self::Attack
+        }
+    }
+}
+
+#[derive(Debug, Clone)]
+struct Piece {
+    player: Player,
+    piecekind: PieceKind,
+}
+
+type Board = Vec<Vec<Option<Piece>>>;
+pub struct Game {
+    turn: Player,
+    board: Board,
+}
+
+impl Game {
+    pub fn setup() -> Self {
+        let mut board = vec![vec![None; 3]; 5];
+        let saru_kick = SARU_KICK.iter().copied().collect();
+        let saru_piece = PieceKind::Saru([-1, 1], saru_kick);
+        let usagi_kick = vec![
+            [1, 0],
+            [2, 0],
+            [0, 1],
+            [0, 2],
+            [0, -1],
+            [0, -2],
+            [1, 1],
+            [2, 2],
+            [1, -1],
+            [2, -2],
+        ];
+        let usagi_piece = PieceKind::Usagi([-1, 1], usagi_kick);
+        let risu_kick = vec![
+            [1, 0],
+            [2, 0],
+            [-1, 0],
+            [-2, 0],
+            [0, 1],
+            [0, 2],
+            [0, -1],
+            [0, -2],
+        ];
+        let risu_piece = PieceKind::Risu([-1, 1], risu_kick);
+        board[1][1] = Some(Piece {
+            player: Player::Attack,
+            piecekind: saru_piece.clone(),
+        });
+        board[0][0] = Some(Piece {
+            player: Player::Attack,
+            piecekind: usagi_piece.clone(),
+        });
+        board[0][2] = Some(Piece {
+            player: Player::Attack,
+            piecekind: risu_piece.clone(),
+        });
+        board[3][1] = Some(Piece {
+            player: Player::Defence,
+            piecekind: saru_piece,
+        });
+        board[4][0] = Some(Piece {
+            player: Player::Defence,
+            piecekind: risu_piece,
+        });
+        board[4][2] = Some(Piece {
+            player: Player::Defence,
+            piecekind: usagi_piece,
+        });
+        board[2][1] = Some(Piece {
+            player: Player::Neutral,
+            piecekind: PieceKind::Ball,
+        });
+        Game {
+            turn: Player::Attack,
+            board,
+        }
+    }
+    fn next_turn(&mut self) {
+        match self.turn {
+            Player::Attack => self.turn = Player::Defence,
+            Player::Defence => self.turn = Player::Attack,
+            _ => (),
+        };
+        for row in &mut self.board {
+            row.reverse();
+        }
+        self.board.reverse();
+    }
+    fn piece_can_move(
+        &self,
+        hei: isize,
+        wid: isize,
+        piece: &Piece,
+    ) -> (Option<Vec<Act>>, Option<Act>) {
+        let mut move_legal: Vec<Act> = vec![];
+        let mut ball_legal: Option<Act> = None;
+        let (idou, _) = piece.piecekind.get().unwrap();
+        for x in hei + idou[0]..=hei + idou[1] {
+            for y in wid + idou[0]..=wid + idou[1] {
+                if (0..=HEIGHT).contains(&x) && (0..=WIDTH).contains(&y) {
+                    match &self.board[x as usize][y as usize] {
+                        None => move_legal.push(Act {
+                            from: (hei, wid),
+                            to: (x, y),
+                            kickto: None,
+                        }),
+                        Some(ball) if ball.piecekind == PieceKind::Ball => {
+                            ball_legal = Some(Act {
+                                from: (hei, wid),
+                                to: (x, y),
+                                kickto: None,
+                            })
+                        }
+                        _ => (),
+                    }
+                }
+            }
+        }
+        if move_legal.is_empty() {
+            (None, ball_legal)
+        } else {
+            (Some(move_legal), ball_legal)
+        }
+    }
+    fn piece_can_kick(
+        &self,
+        ball_legal: &Act,
+        kicked_mask: Vec<(isize, isize)>,
+        piece: &Piece,
+    ) -> Vec<Act> {
+        let (_, kickto) = piece.piecekind.get().unwrap();
+        let mut kicks_legal: Vec<Act> = vec![];
+        for kick in kickto {
+            let (x, y) = (ball_legal.to.0 + kick[0], ball_legal.to.1 + kick[1]);
+            if (-1 <= x && x <= HEIGHT) && (0..=WIDTH).contains(&y) {
+                if (x == -1 || x == 5) || ((x, y) == ball_legal.from) {
+                    kicks_legal.push(Act {
+                        from: ball_legal.from,
+                        to: ball_legal.to,
+                        kickto: Some((x, y)),
+                    });
+                } else if !kicked_mask.iter().any(|&kicked| kicked == (x, y)) {
+                    match &self.board[x as usize][y as usize] {
+                        Some(found_piece) if found_piece.player == self.turn => {
+                            let mut kicked_mask_chi = kicked_mask.clone();
+                            kicked_mask_chi.push((x, y));
+                            kicks_legal.append(&mut self.piece_can_kick(
+                                ball_legal,
+                                kicked_mask_chi,
+                                found_piece,
+                            ));
+                        }
+                        None => kicks_legal.push(Act {
+                            from: ball_legal.from,
+                            to: ball_legal.to,
+                            kickto: Some((x, y)),
+                        }),
+                        _ => (),
+                    }
+                }
+            }
+        }
+        kicks_legal
+    }
+    fn piece_legal_move(&self, hei: isize, wid: isize, piece: &Piece) -> Vec<Act> {
+        let (move_legal, ball_legal) = self.piece_can_move(hei, wid, piece);
+        let mut legal = match move_legal {
+            Some(moves) => moves,
+            None => vec![],
+        };
+        if let Some(ball_legal) = ball_legal {
+            legal.append(&mut self.piece_can_kick(&ball_legal, vec![ball_legal.to], piece))
+        }
+        legal
+    }
+    fn legal_moves(&self) -> Vec<Act> {
+        let mut ret: Vec<Act> = vec![];
+        for (hei, row) in self.board.iter().enumerate() {
+            for (wid, col) in row.iter().enumerate() {
+                match col {
+                    Some(piece) => {
+                        ret.append(&mut self.piece_legal_move(hei as isize, wid as isize, piece))
+                    }
+                    None => (),
+                }
+            }
+        }
+        ret
+    }
+    // fn reset();
+    fn action_parse(&mut self, act: Option<Act>) -> (bool, Option<Player>) {
+        // パスしかできなかった
+        if act == None {
+            self.next_turn();
+            return (true, None);
+        }
+        // ただしくないact
+        let act = act.unwrap();
+        let legalmoves = self.legal_moves();
+        if !legalmoves.iter().any(|&x| x == act) {
+            return (false, None);
+        }
+        // 正しいactであれば移動を実行
+        // もちろんキックも同時に実行　ここで勝利判定
+        match act.kickto {
+            Some(kick) => {
+                let last_stop = kick.0;
+                if last_stop == 0 - 1 {
+                    return (true, Some(self.turn.against()));
+                } else if last_stop == HEIGHT + 1 {
+                    return (true, Some(self.turn));
+                }
+                let mut tmp = None;
+                std::mem::swap(
+                    &mut self.board[act.to.0 as usize][act.to.1 as usize], // here None
+                    &mut tmp,                                              // here ball
+                );
+                std::mem::swap(
+                    &mut self.board[kick.0 as usize][kick.1 as usize], // here ball
+                    &mut tmp,                                          // here None
+                );
+                std::mem::swap(
+                    &mut self.board[act.from.0 as usize][act.from.1 as usize], // here None
+                    &mut tmp,                                                  // here piece
+                );
+                std::mem::swap(
+                    &mut self.board[act.to.0 as usize][act.to.1 as usize], // here piece
+                    &mut tmp,                                              // here None
+                );
+            }
+            None => {
+                let mut tmp = None;
+                std::mem::swap(
+                    &mut self.board[act.from.0 as usize][act.from.1 as usize], // here None
+                    &mut tmp,                                                  // here Piece
+                );
+                std::mem::swap(
+                    &mut self.board[act.to.0 as usize][act.to.1 as usize], // here Piece
+                    &mut tmp,                                              // here None
+                );
+            }
+        }
+        // 親猿にグレードアップ
+        // turn変更
+        if act.to.0 == 4 {
+            if let PieceKind::Oyasaru(_, _) = self.board[act.to.0 as usize][act.to.1 as usize]
+                .as_ref()
+                .unwrap()
+                .piecekind
+            {
+                self.board[act.to.0 as usize][act.to.1 as usize] = Some(Piece {
+                    player: self.turn,
+                    piecekind: PieceKind::Oyasaru([-2, 2], SARU_KICK.iter().copied().collect()),
+                });
+            }
+        }
+        self.next_turn();
+        // 成功をreturn
+        (true, None)
+    }
+    pub fn game<T: Agent>(agent1: T, agent2: T) -> String {
+        let mut game_ins = Self::setup();
+        let turn = rand::thread_rng().gen_range(0..=1);
+        let ((front, back), (frontstr, backstr)) = if turn == 0 {
+            ((agent1, agent2), ("agent1", "agent2"))
+        } else {
+            ((agent2, agent1), ("agent2", "agent1"))
+        };
+        let mut now_player = (true, &front);
+        loop {
+            let mut winner: Option<Player>;
+            loop {
+                let action: Option<Act>;
+                if game_ins.legal_moves().is_empty() {
+                    action = None;
+                } else {
+                    action = Some(now_player.1.action(&game_ins));
+                }
+                let (success, winnertmp) = game_ins.action_parse(action);
+                winner = winnertmp;
+                if success {
+                    break;
+                }
+            }
+            if let Some(player) = winner {
+                match player {
+                    Player::Attack => {
+                        if now_player.0 {
+                            return format!("{} win", frontstr);
+                        } else {
+                            return format!("{} win", backstr);
+                        }
+                    }
+                    Player::Defence => {
+                        if now_player.0 {
+                            return format!("{} win", backstr);
+                        } else {
+                            return format!("{} win", frontstr);
+                        }
+                    }
+                    _ => (),
+                }
+            }
+            now_player = if now_player.0 {
+                (false, &back)
+            } else {
+                (true, &front)
+            };
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    #[test]
+    fn piece_can_move_works() {
+        let game = Game::setup();
+        let input = vec![(1, 1, game.board[1][1].as_ref().unwrap())];
+        let move_legal_seed = vec![(2, 0), (2, 2), (1, 0), (1, 2), (0, 1)];
+        let mut move_legal_unwrap: Vec<Act> = vec![];
+        for moves in move_legal_seed {
+            move_legal_unwrap.push(Act {
+                from: (1, 1),
+                to: moves,
+                kickto: None,
+            })
+        }
+        let ball_legal_unwrap = Act {
+            from: (1, 1),
+            to: (2, 1),
+            kickto: None,
+        };
+        let output = vec![(move_legal_unwrap, ball_legal_unwrap)];
+        for (inp, mut ref_out) in input.into_iter().zip(output.into_iter()) {
+            let (move_legal, ball_legal) = game.piece_can_move(inp.0, inp.1, inp.2);
+            assert_eq!(ref_out.0.sort(), move_legal.unwrap().sort());
+            assert_eq!(ref_out.1, ball_legal.unwrap());
+        }
+    }
+}
